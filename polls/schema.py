9f17285dba533c8f244f7b4af8af2f73fbd8b828
Shubham Gupta <shubham.vimal@neosoftmail.com>
2021-07-08 20:12:11 +0530

    added schema

--- polls/schema.py
+++ polls/schema.py
@@ -1,3 +1,5 @@
+import datetime
+
 import graphene
 from graphene_django import DjangoObjectType
 
@@ -31,4 +33,63 @@ class Query(graphene.ObjectType):
             return None
 
 
-schema = graphene.Schema(query=Query)
+class QuestionInput(graphene.InputObjectType):
+    id = graphene.ID()
+    question_text = graphene.String()
+    pub_date = graphene.DateTime()
+
+
+class CreateQuestion(graphene.Mutation):
+    class Arguments:
+        question_data = QuestionInput(required=True)
+
+    question = graphene.Field(QuestionSchema)
+
+    @staticmethod
+    def mutate(root, info, question_data=None):
+        q_instance = Question(
+            question_text=question_data.question_text,
+            pub_date=datetime.datetime.now(),
+        )
+        q_instance.save()
+        return CreateQuestion(question=q_instance)
+
+
+class UpdateQuestion(graphene.Mutation):
+    class Arguments:
+        question_data = QuestionInput(required=True)
+
+    question = graphene.Field(QuestionSchema)
+
+    @staticmethod
+    def mutate(root, info, question_data=None):
+        print(question_data.question_text)
+        q_instance = Question.objects.get(pk=question_data.id)
+        if q_instance:
+            q_instance.question_text = question_data.question_text
+            q_instance.save()
+            return UpdateQuestion(question=q_instance)
+        return UpdateQuestion(book=None)
+
+
+class DeleteQuestion(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    question = graphene.Field(QuestionSchema)
+
+    @staticmethod
+    def mutate(root, info, id):
+        question_instance = Question.objects.get(pk=id)
+        question_instance.delete()
+
+        return None
+
+
+class Mutation(graphene.ObjectType):
+    create_question = CreateQuestion.Field()
+    update_question = UpdateQuestion.Field()
+    delete_question = DeleteQuestion.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
